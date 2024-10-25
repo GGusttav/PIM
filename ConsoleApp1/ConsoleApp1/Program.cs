ff26653fdd9784502984fc4436c933c801e307d1
GGusttav <160732172+GGusttav@users.noreply.github.com>
2024-10-25 19:58:18 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+{ public class TimePeriod
+{
+    private double _seconds;
+
+    public double Hours
+    {
+        get { return _seconds / 3600; }
+        set
+        {
+            if (value < 0 || value > 24)
+                throw new ArgumentOutOfRangeException(nameof(value),
+                      "The valid range is between 0 and 24.");
+
+            _seconds = value * 3600;
+        }
+    }
+}
